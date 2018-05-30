9a38c022f1dab6c356fc9281e49fb38d3deb6a85
Larissa <llo@cesar.org.br>
2018-05-30 08:36:35 -0300

    add connection to mongodb

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser= require('body-parser')
 
-app.listen(8081, function() {
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect('mongodb://llo3:abc1234@ds139960.mlab.com:39960/knot_db', (err, client) => { //your MongoDB url
+  if (err) return console.log(err)
+
+  var db = client.db('knot_db') //name of your database
+
+  app.listen(8081, () => {
     console.log('listening on 8081')
   })
+})
 
-  app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-  })
-  app.post('/getDevices', (req, res) => {
-    console.log('Test')
-  })
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/getDevices', (req, res) => {
+  console.log(req.body)
+})
